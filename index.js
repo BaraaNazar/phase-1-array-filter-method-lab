d78daa06e36d96509464b63de5704976f559d1ae
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-11-23 15:51:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(names,string){
+return names.filter(names => names.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(names,string){
+  return names.filter(names => names.startsWith(string))
+
+}
+function matchName(driver,string){
+return driver.filter(driver => driver.name.includes(string))
+}
